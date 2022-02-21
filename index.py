42fc80ceb13004c3d3e63d5f13a59fdcf9c97217
oded azoulay <dogshouseisrael@gmail.com>
2022-02-21 19:53:38 +0200

    added python web server

--- index.py
+++ index.py
@@ -1,6 +1,41 @@
-import requests
-api_url = "https://jsonplaceholder.typicode.com/todos/1"
-response = requests.get(api_url)
-response.json()
-print(response.json())
-print("END")
+# import requests
+# import sys
+# sys.path.insert(0, '/Users/oded/Desktop/projects/python/home-assignment/controllers')
+# import githubController
+# sys.path.insert(0, '/Users/oded/Desktop/projects/python/home-assignment/handlers')
+# import githubhandler
+
+# api_url = "https://jsonplaceholder.typicode.com/todos/1"
+# response = requests.get(api_url)
+# response.json()
+# print(response.json())
+# print("END")
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
